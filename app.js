601e4514b5d210b6a24ad5b13a34ecf7e8d54021
Ahmed <ahmed.max7586@gmail.com>
2019-08-21 15:52:35 -0400

    creat sort test

--- app.js
+++ app.js
@@ -2,9 +2,7 @@
 
 const express = require('express');
 const morgan = require('morgan');
-
 const playstore = require('./playstore');
-
 const app = express();
 
 app.use(morgan('dev'));
---
+++ test/sort.test.js
@@ -0,0 +1,21 @@
+const { sortStuff } = require("../app");
+const expect = require("chai").expect;
+
+describe("sort the list by either rating or app ", () => {
+
+});
+
+const dummyData = [
+    {
+        "App": "ROBLOX",
+        "Rating": 4.5
+    },
+    {
+        "App": "Subway Surfers",
+        "Rating": 4.5,
+    },
+    {
+        "App": "Candy Crush Saga",
+        "Rating": 4.4,
+    },
+];
